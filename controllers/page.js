da59c7392a6c4c1e9d595cdaa948f0891ac06cb9
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-03-30 19:49:47 +0700

    push

--- controllers/page.js
+++ controllers/page.js
@@ -1,8 +1,19 @@
 //home page
 export const Home = (req, res) => {
-    res.send('Selamat datang di homepage');
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+
+    res.render('../pages/index', {
+        mascots: mascots,
+        tagline: tagline
+    });
 }
 
 export const About = (req, res) => {
     res.send('Halaman tentang');
+
 } 
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 //import express
 import express from "express";
 //import routes
@@ -8,6 +6,8 @@ import Router from "./routes/routes.js";
 const app = express();
 
 app.use(Router);
+
+app.set('view engine', 'ejs');
 
 //listen on port
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
